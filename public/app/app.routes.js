6f64bd764d910d143579e70c2131e3e9fa29e0e4
Winson <winsonyuan1@gmail.com>
2015-05-16 13:22:48 -0400

    all public stuff

---
+++ public/app/app.routes.js
@@ -0,0 +1,42 @@
+angular.module( 'app.routes', [ 'ngRoute' ])
+
+	.config( function( $routeProvider, $locationProvider ) {
+		// home page route
+		$routeProvider
+		.when( '/', {
+			templateUrl: 'app/views/pages/home.html'
+		})
+
+		// login page
+		.when( '/login', {
+			templateUrl: 'app/views/pages/login.html'
+			,controller: 'mainController'
+			,controllerAs: 'login'
+		})
+
+		// show all users
+		.when( '/users', {
+			templateUrl: 'app/views/pages/users/all.html'
+			,controller: 'userController'
+			,controllerAs: 'user'
+		})
+
+		// form to create a new user
+		// same view as edit page
+		.when( '/users/create', {
+			templateUrl: 'app/views/pages/users/single.html'
+			,controller: 'userCreateController'
+			,controllerAs: 'user'
+		})
+
+		// page to edit a user
+		.when( '/user/:user_id', {
+			templateUrl: 'app/views/pages/users/single.html'
+			,controller: 'userEditController'
+			,controllerAs: 'user'
+		});
+
+		// get rid of the hash in the URL
+		$locationProvider.html5Mode( true );
+
+	});
